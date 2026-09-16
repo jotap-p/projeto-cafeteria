6d44dcd435970b95f82efcc8e854cf8dbfb77e9a
jotap-p <jotap.cout@gmail.com>
2026-09-16 17:37:48 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Cafeteria - Trabalho da Faculdade</h1>
+      <p>Seja bem-vindo ao nosso e-commerce de café!</p>
+    </div>
+  );
+}
+
+export default App;
