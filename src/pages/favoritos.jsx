ce742f4dbb40709669f3b5bb35458359dd3b41bb
lauramelominarde-blip <lauramelominarde@gmail.com>
2026-09-23 20:37:15 -0300

    criei a pagina de favoritos

---
+++ src/pages/favoritos.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function Favoritos() {
+  const [favoritos, setFavoritos] = useState([]);
+
+  if(favoritos.length === 0) 
+  return (
+    <div>
+        <h1>Favoritos</h1>
+
+        <p>
+            Voce ainda não possui favoritos.
+        </p>
+    </div>
+  );
+}
+    
+return (
+  <div>
+    <h1>Favoritos</h1>
+
+    {favoritos.map((produto=>(
+        <div key={produto.id}>
+            <h2>{produto.nome}</h2>
+
+            <p>{produto.descricao}</p>
+
+            <p>{produto.preco}</p>
+            </div>
+    )))}
+  </div>
+);
+
+ 
